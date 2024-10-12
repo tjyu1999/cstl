68441dd80b4efc418c9c62a5cfaa3fe2fc57b142
tjyu1999 <tjyu1999@gmail.com>
2024-10-13 01:03:20 +0800

    add memchr.c

---
+++ string/memchr.c
@@ -0,0 +1,15 @@
+/* memchr function: locate character in block of memory */
+
+#include <string.h>
+
+void *memchr(const void *s, int c, size_t n) {
+    const unsigned char uc = c;
+    const unsigned char *su = (const unsigned char *)s;
+    
+    for (;  0 < n; ++su, --n) {
+        if (*su == uc)
+            return (void *)su;
+    }
+    
+    return NULL;    
+}
