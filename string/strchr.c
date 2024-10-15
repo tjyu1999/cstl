09a20f540c67889c0aebceef430dce405a9b81d2
tjyu1999 <tjyu1999@gmail.com>
2024-10-16 00:14:28 +0800

    add string/strchr.c

---
+++ string/strchr.c
@@ -0,0 +1,14 @@
+/* strchr function: locate first occurrence of character in string */
+
+#include <string.h>
+
+const char *strchr(const char *s, int c) {
+    const char ch = c;
+    
+    for (; *s != ch; ++s) {
+        if (*s == '\0')
+            return NULL;
+    }
+    
+    return (char *)s;
+}
