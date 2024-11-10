eb8ed5fb8720b9d6fd4be82fce6616bcebdac734
tjyu1999 <tjyu1999@gmail.com>
2024-11-11 01:03:08 +0800

    edit string/memchr.c

--- string/memchr.c
+++ string/memchr.c
@@ -4,11 +4,11 @@
 
 void *memchr(const void *s, int c, size_t n) {
     const unsigned char uc = c;
-    const unsigned char *su = (const unsigned char *)s;
+    const unsigned char *su = (const unsigned char *) s;
     
     for (;  0 < n; ++su, --n) {
         if (*su == uc)
-            return (void *)su;
+            return (void *) su;
     }
     
     return NULL;    
