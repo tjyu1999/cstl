fe147fa288b5bbe444cb1a2ad4d3495435831d2b
tjyu1999 <tjyu1999@gmail.com>
2024-11-10 11:59:28 +0800

    edit string/strcmp.c

--- string/strcmp.c
+++ string/strcmp.c
@@ -1,7 +1,7 @@
 /* strcmp function: compare two strings */
 
 int strcmp(const char *s1, const char *s2) {
-    for (;*s1 == *s2; ++s1, ++s2) {
+    for (; *s1 == *s2; ++s1, ++s2) {
         if (*s1 =='\0')
             return 0;
     }
