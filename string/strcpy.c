4cc88582500804faf3b401a6c4d69279d76cdd93
tjyu1999 <tjyu1999@gmail.com>
2024-11-18 16:21:49 +0800

    edit string/strcpy.c

--- string/strcpy.c
+++ string/strcpy.c
@@ -5,7 +5,7 @@
 char *strcpy(char *s1, const char *s2) {
     char *s = s1;
     
-    for(s = s1; (*s++ = *s2++) != '\0';)
+    for (s = s1; (*s++ = *s2++) != '\0';)
         ;
     
     return s1;
